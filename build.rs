08f54cd73a6a128bf9af9d1bb3384f41603e1f86
asShidqi <fayyed76@gmail.com>
2025-05-08 10:40:41 +0700

    Payment Service Implementation

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .compile(
-            &["proto/service.proto"],
+            &["proto/services.proto"],
             &["proto"],
         )?;
     Ok(())
